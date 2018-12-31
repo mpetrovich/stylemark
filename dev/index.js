51f88d5ccbd92fa9b5e8b1442c2325abf8ca9810
Mike Petrovich <michael.c.petrovich@gmail.com>
2018-12-30 21:16:52 -0500

    WIP

--- dev/index.js
+++ dev/index.js
@@ -5,12 +5,13 @@ const flatMap = require('lodash/flatMap');
 const unified = require('unified');
 const markdownParser = require('remark-parse');
 const codeBlockParser = require('./codeblocks');
-const iframer = require('./iframer');
 const frontmatterParser = require('remark-frontmatter');
 const frontmatterExtractor = require('remark-extract-frontmatter');
 const yaml = require('yaml').parse;
 const remark2rehype = require('remark-rehype');
 const htmlRenderer = require('rehype-stringify');
+const visit = require('unist-util-visit');
+const is = require('hast-util-is-element');
 
 (async function() {
 	try {
@@ -68,14 +69,43 @@ function createDoc(markdown, filepath) {
 }
 
 async function htmlDocWriter(doc, outputDir) {
+	const renderableExtensions = ['html', 'jsx'];
 	const rendered = unified()
 		.use(markdownParser)
-		.use(iframer, (lang, index) => `examples/${doc.id}/${index}-${lang}`)
+		.use(() => (tree, file) => {
+			let iframeCount = 0;
+			file.data.iframes = file.data.iframes || [];
+
+			visit(tree, 'code', (node, index, parent) => {
+				const isRenderable = renderableExtensions.some(ext => node.lang.endsWith(ext));
+
+				if (!isRenderable) {
+					return;
+				}
+
+				const src = `examples/${doc.id}/${iframeCount++}-${node.lang}`;
+				const iframe = {
+					type: 'element',
+					data: {
+						hName: 'iframe',
+						hProperties: { src }
+					}
+				};
+
+				file.data.iframes.push(src);
+
+				parent.children = [].concat(
+					parent.children.slice(0, index),
+					iframe,
+					parent.children.slice(index),
+				);
+			});
+		})
 		.use(remark2rehype)
 		.use(htmlRenderer)
 		.processSync(doc.markdown);
 
-	const { iframes } = rendered.data;
+	const { iframes = [] } = rendered.data;
 	const html = rendered.toString();
 
 	await fs.outputFile(path.resolve(outputDir, `${doc.id}.html`), html, 'utf8');
