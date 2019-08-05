1fb6d477c8df422af35122c59dea1cf98b3ee07c
Mike Petrovich <michael.c.petrovich@gmail.com>
2019-08-05 01:07:16 -0400

    WIP

--- src/extractSpecimenBlocks.js
+++ src/extractSpecimenBlocks.js
@@ -58,7 +58,7 @@ module.exports = ({ importLoader }) => (tree, file) => {
 
 		specimenBlocks.push({
 			specimenName,
-			language: language,
+			language,
 			flags,
 			props,
 			content: contentWithoutFrontmatterOrImports,
