6dc27c2a2a4aed0e0d4b07a3a6019a2be8b8c89a
Mike Petrovich <michael.c.petrovich@gmail.com>
2019-08-04 23:56:52 -0400

    WIP

--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,13 @@ const fs = require('fs')
 const path = require('path')
 const extractComponent = require('./extractComponent')
 const specimenRenderer = require('./specimenRenderer')
+const blockRenderer = require('./blockRenderer')
 
 const sourcePath = path.resolve(__dirname, '../docs/example.md')
 const outputPath = path.resolve(__dirname, '../dist/index.html')
 
 const markdown = fs.readFileSync(sourcePath)
 const component = extractComponent(markdown, { importLoader: f => '' })
-const html = specimenRenderer(component.specimens[0])
+const html = specimenRenderer(component.specimens[0], { blockRenderer })
 
 fs.writeFileSync(outputPath, html)
