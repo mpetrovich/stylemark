7943d36bff157fb030efdd0a518ac8dc74fc9e41
Mike Petrovich <michael.c.petrovich@gmail.com>
2020-02-22 02:49:30 +0100

    Attach blocks in order of definition

--- src/render/attachSpecimen.js
+++ src/render/attachSpecimen.js
@@ -2,26 +2,19 @@ function attachSpecimen(selector, specimen) {
     const host = document.querySelector(selector)
     const shadow = host.attachShadow({ mode: "open" })
 
-    shadow.innerHTML = specimen.blocks
-        .filter(block => block.language === "html")
-        .map(block => block.content)
-        .join("\n")
-
-    specimen.blocks
-        .filter(block => block.language === "css")
-        .forEach(block => {
+    specimen.blocks.forEach(block => {
+        if (block.language === "html") {
+            shadow.innerHTML += block.content
+        } else if (block.language === "css") {
             const style = document.createElement("style")
             style.type = "text/css"
             style.textContent = block.content
             shadow.appendChild(style)
-        })
-
-    specimen.blocks
-        .filter(block => block.language === "js")
-        .forEach(block => {
+        } else if (block.language === "js") {
             const script = document.createElement("script")
             script.type = "text/javascript"
             script.textContent = block.content
             shadow.appendChild(script)
-        })
+        }
+    })
 }
