291301249b2af73a9c456ae2c3a313b0dcf7a2b8
Mike Petrovich <michael.c.petrovich@gmail.com>
2020-02-26 02:44:02 +0100

    Move specimen init script to assets

--- src/compile/compileLibrary.js
+++ src/compile/compileLibrary.js
@@ -2,11 +2,15 @@ const fs = require("fs")
 const path = require("path")
 const compileComponent = require("./compileComponent")
 
+const initSpecimenEmbedScript = fs.readFileSync(path.resolve(__dirname, "..", "assets", "initializeSpecimenEmbed.js"), {
+    encoding: "utf8",
+})
+
 module.exports = library => `<!doctype html>
 <html>
 <head>
     <title>${library.name}</title>
-	<script>${fs.readFileSync(path.resolve(__dirname, "initializeSpecimenEmbed.js"), { encoding: "utf8" })}</script>
+	<script>${initSpecimenEmbedScript}</script>
 </head>
 <body>
 	${library.components.map(compileComponent).join("")}
