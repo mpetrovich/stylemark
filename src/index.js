f376370083361a9e54c723551ee0a3ca0cbd72f0
Mike Petrovich <michael.c.petrovich@gmail.com>
2020-04-26 00:25:40 +0200

    Improve readability

--- src/index.js
+++ src/index.js
@@ -9,15 +9,16 @@ const parseComponent = require("./parse/parseComponent")
 const compileLibrary = require("./compile/compileLibrary")
 
 module.exports = ({ input, output, name, cwd }) => {
-    const filepaths = getMatchingFiles(input, cwd)
-    const components = _.flatMap(filepaths, filepath => {
+    const inputFilepaths = getMatchingFiles(input, cwd)
+    const components = _.flatMap(inputFilepaths, filepath => {
         const content = fs.readFileSync(filepath, { encoding: "utf8" })
         const isMarkdownFile = filepath.endsWith(".md") || filepath.endsWith(".markdown")
         return isMarkdownFile ? parseComponent(content) : extractCommentBlocks(content).map(parseComponent)
     })
     const library = new Library({ name, components })
-
     const html = compileLibrary(library)
+
+    const outputFilepath = path.join(output, "index.html")
     mkdirp(output)
-    fs.writeFileSync(path.join(output, "index.html"), html)
+    fs.writeFileSync(outputFilepath, html)
 }
